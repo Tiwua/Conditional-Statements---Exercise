25804ef6e3b869045b6a84c0a55f6337a97b2e82
Valio <valentin.gaidarov@gmail.com>
2022-05-29 19:33:52 +0300

    Conditional Statements - Exercise

---
+++ GodzillaVsKong/GodzillaVsKong.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06.GodzillaVsKong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int countStatists = int.Parse(Console.ReadLine());
+            double pricePerStatist = double.Parse(Console.ReadLine());
+
+            double decorPrice = 0.10 * budget;
+            double clothesPrice = countStatists * pricePerStatist;
+
+            if (countStatists > 150)
+            {
+                clothesPrice = clothesPrice * 0.9;
+            }
+
+            double expenses = decorPrice + clothesPrice;
+
+            if (budget >= expenses)
+            {
+                Console.WriteLine("Action!");
+                double leftMoney = budget - expenses;
+                Console.WriteLine($"Wingard starts filming with {leftMoney:F2} leva left.");
+            }
+            else if (expenses > budget)
+            {
+                Console.WriteLine("Not enough money!");
+                double needMoney = expenses - budget;
+                Console.WriteLine($"Wingard needs {needMoney:F2} leva more.");
+            }
+        }
+    }
+}
---
+++ SwimRecord/SwimRecord.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SwimRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                double record = double.Parse(Console.ReadLine());
+                double meters = double.Parse(Console.ReadLine());
+                double timePerMeter = double.Parse(Console.ReadLine());
+
+                double swimTime = meters * timePerMeter;
+                double delay = Math.Floor(meters / 15) * 12.5;
+
+                double totalTime = swimTime + delay;
+
+                if (totalTime < record)
+                {
+                    Console.WriteLine($" Yes, he succeeded! The new world record is {totalTime:F2} seconds.");
+                }
+
+                else 
+                {
+                    double secondsNeeded = totalTime - record;
+                    Console.WriteLine($"No, he failed! He was {secondsNeeded:F2} seconds slower.");
+                }
+            }  
+        }
+    }
+}
