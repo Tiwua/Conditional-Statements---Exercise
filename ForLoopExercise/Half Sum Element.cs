a4edd105f97c47a85dc6bc0526dc23bbe3c7003e
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2022-06-01 12:28:08 +0300

    Add files via upload

---
+++ ForLoopExercise/Half Sum Element.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HalfSumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfRows = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int max = int.MinValue;
+
+
+            for (int n = 0; n < numOfRows; n++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+
+            int sumWithoutMaxNumber = sum - max;
+            if (max == sumWithoutMaxNumber)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = " + max);
+            }
+            else
+            {
+                int diff = Math.Abs(max - sumWithoutMaxNumber);
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = " + diff);
+            }
+
+        }
+    }
+}
