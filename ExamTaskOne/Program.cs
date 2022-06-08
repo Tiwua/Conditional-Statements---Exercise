40005bb06d4d363c8a9ac21cb67fb8ec4e8841bc
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2022-06-08 22:13:26 +0300

    Add files via upload

---
+++ ExamTaskOne/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExamTaskOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            double headsetPrice = int.Parse(Console.ReadLine());
+            double mousePrice = int.Parse(Console.ReadLine());
+            double keyboardPrice = int.Parse(Console.ReadLine());
+            double displayPrice = int.Parse(Console.ReadLine());
+
+
+            double headsetCost = (lostGames / 2) * headsetPrice;
+            double miceCost = (lostGames / 3) * mousePrice;
+            double keyboardCost = (lostGames / 6) * keyboardPrice;
+            double displayCost = (lostGames / 12) * displayPrice;
+            double expenses = headsetCost + miceCost + keyboardCost + displayCost;
+
+            Console.WriteLine($"Rage expenses: {expenses:f2} lv.");
+        }
+    }
+}
